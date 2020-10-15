6323f98e6b91ae4b5aa92ce1f63307459e1c04f9
Paulo Mol <pmolufmg@gmail.com>
2020-10-14 21:12:51 -0300

    Refatorando o Teste Funcional

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,57 +1,10 @@
 #!/usr/bin/python3
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-import time
 
-MAX_WAIT = 5
-
-class NewVisitorTest(StaticLiveServerTestCase):
+class NewVisitorTest(FunctionalTest):
     
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:  
-            try:
-                table = self.browser.find_element_by_id("id_list_table")
-                rows = table.find_elements_by_tag_name("tr")
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            
-            except(AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-    
-    def test_layout_and_styling(self):
-        
-        # Edith entra na home 
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # Ela nota que o input box está centralizado
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width']/ 2,
-            512,
-            delta=10)
-
-        # Ela inicia uma nova lista e nota que o input também está centralizado
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width']/ 2,
-            512,
-            delta=10)
-   
     def test_can_start_a_list_for_one_user(self): 
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
         self.browser.get(self.live_server_url)
