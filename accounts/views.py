fa0ff1ee334836e01e374d00b7323d7d35ec2acb
Paulo Mol <pmolufmg@gmail.com>
2020-10-25 11:35:42 -0300

    Testando o Framework de mensagens do Django

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.shortcuts import redirect
+from django.contrib import messages
 
 def send_login_email(request):
-    email = request.POST['email']
+    email = request.POST["email"]
     send_mail(
-        'Your login link for Superlists',
-        'body text tbc',
-        'noreply@superlists',
+        "Your login link for Superlists",
+        "body text tbc",
+        "noreply@superlists",
         [email],)
-    
-    return redirect('/')
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect("/")
